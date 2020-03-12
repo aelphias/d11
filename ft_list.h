06063f308dc805c8f812935087c5277365df4164
aelphias <igormitchell11@gmail.com>
2020-03-12 20:31:14 +0300

    1st

---
+++ ft_list.h
@@ -0,0 +1,16 @@
+//
+// Created by io on 02.10.2019.
+//
+
+#ifndef D11_FT_LIST_H
+#define D11_FT_LIST_H
+
+typedef struct		s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+
+
+#endif //D11_FT_LIST_H
---
+++ ft_list_push_back.c
@@ -0,0 +1,7 @@
+//
+// Created by io on 02.10.2019.
+//
+void	ft_list_push_back(t_list **begin_list, void *data)
+{
+    
+}
